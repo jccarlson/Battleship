12069f07410d4eba98c05ec5ab6b085b9d8dd7e6
Jason Carlson <jcarlson08@gmail.com>
2015-09-08 14:20:16 -0600

    minor update - no change in functionality
    Utilizes the AudioSystem.getClip method instead of typecasting a
    DataLine, and imports less classes (i.e. only those necessary) from
    javax.sound compared to v1.0.

--- src/sound/WavPlayer.java
+++ src/sound/WavPlayer.java
@@ -2,15 +2,21 @@ package sound;
 
 import java.io.IOException;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+
 
 /**
  * Provides a simple player for *.wav files.
  * 
  * @author Jason Carlson <jccarlson@miners.utep.edu>
- * @version 1.0
+ * @version 1.1
  * @since 2015-09-03
- *
  */
 public class WavPlayer implements LineListener {
 
@@ -28,9 +34,9 @@ public class WavPlayer implements LineListener {
 		try {
 			isComplete = false;
 			AudioInputStream aStream = AudioSystem.getAudioInputStream(WavPlayer.class.getResource(filename));
-			AudioFormat f = aStream.getFormat();
-			DataLine.Info i = new DataLine.Info(Clip.class, f);
-			Clip c = (Clip) AudioSystem.getLine(i);
+			//AudioFormat f = aStream.getFormat();
+			//DataLine.Info i = new DataLine.Info(Clip.class, f);
+			Clip c = (Clip) AudioSystem.getClip();
 
 			c.addLineListener(this);
 
